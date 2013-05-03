f54dd8808e320e1f83829d16fadc343ccf13177d
Prashanth Pereira <prashanth.pereira@gmail.com>
2013-05-03 02:15:42 -0400

    new

--- js/maps.js
+++ js/maps.js
@@ -52,12 +52,11 @@ Map.displayMap = function(userPosition, carPosition)
     
    google.maps.event.addListener(marker, 'click', function() {
 	   infowindow.open(map, marker);
-	  /* $(".clickeventvialive").bind("click", function (){		  
-		   $.mobile.changePage('directions.html');
-		   });*/
-	   $('.clickeventvialive').click( function() { 
-	        $.mobile.changePage($('#page-map'), {});
-	    });
+	   $(".clickeventvialive").bind("click", function (){		  
+		  // $.mobile.changePage('directions.html');
+		   $.mobile.changePage($('#page-map'), {});
+		   });
+	  
 	 });
    
   
