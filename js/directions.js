e40185899c36713f086999695a9a04002511ff56
Prashanth Pereira <prashanth.pereira@gmail.com>
2013-05-04 01:23:06 -0400

    d

--- js/directions.js
+++ js/directions.js
@@ -137,9 +137,15 @@ $('#dir_panel').live("tap", function() {
  $.mobile.changePage($('#page-map'), {});
 });
 
+$('#directions-image').live("tap", function() {
+	 $.mobile.changePage($('#page-map'), {});
+	});
+
+
 $('.directions').live("tap", function() {
 	 $.mobile.changePage("directions2.html");
 	});
+
 
 //Briefly show hint on using instruction tap/zoom
 $('#page-dir').live("pageshow", function() {
